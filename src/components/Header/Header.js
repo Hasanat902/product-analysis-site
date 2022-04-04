0d89c954af8b409f3dfb357eb58a07bf1f2ac479
Hasanat902 <abulhasanat902@gmail.com>
2022-04-04 13:04:26 +0600

    create fake data

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,11 @@ const Header = () => {
     return (
         <div>
             <nav className='text-center mt-3'>
-                <Link className='mr-5 text-blue-400 font-bold' to='/home'>HOME</Link>
-                <Link className='mr-5 text-blue-400 font-bold' to='/reviews'>REVIEWS</Link>
-                <Link className='mr-5 text-blue-400 font-bold' to='/dashboard'>DASHBOARD</Link>
-                <Link className='mr-5 text-blue-400 font-bold' to='/blogs'>BLOGS</Link>
-                <Link className='mr-5 text-blue-400 font-bold' to='/about'>ABOUT</Link>
+                <Link className='mr-5 text-blue-400' to='/home'>HOME</Link>
+                <Link className='mr-5 text-blue-400' to='/reviews'>REVIEWS</Link>
+                <Link className='mr-5 text-blue-400' to='/dashboard'>DASHBOARD</Link>
+                <Link className='mr-5 text-blue-400' to='/blogs'>BLOGS</Link>
+                <Link className='mr-5 text-blue-400' to='/about'>ABOUT</Link>
             </nav>
         </div>
     );
